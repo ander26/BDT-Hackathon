fca22c185425b98197100ea809b8e40ab689dce8
Ander Arguiñano <ander.arguinano@opendeusto.es>
2021-06-12 11:21:18 +0200

    Deleted unnecessary content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,49 @@
-import '../styles/globals.css'
+import { NextSeo } from "next-seo";
+import { fonts } from "styles/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextSeo
+        title="BDT Hackathon Nuwe"
+        description="Desarrollo realizado para el Hackathon impulsado por Nuwe"
+        canonical="https://bdt-hackathon.vercel.app/"
+        openGraph={{
+          url: "https://bdt-hackathon.vercel.app/",
+          title: "BDT Hackathon Nuwe",
+          description:
+            "Desarrollo realizado para el Hackathon impulsado por Nuwe",
+          images: [
+            {
+              url: "https://media-exp1.licdn.com/dms/image/C4E1BAQE7oS5DoVCTow/company-background_10000/0/1614961937859?e=2159024400&v=beta&t=5hBIvSEDk1LaBKgYtO176BQ-KCgMg36qc9nmdkxNAqk",
+              width: 1130,
+              height: 191,
+              alt: "Encabezado Nuwe",
+            },
+          ],
+          site_name: "BDT Hackathon Nuwe",
+        }}
+      />
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: ${fonts.base};
+          }
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      </style>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.js
@@ -0,0 +1,3 @@
+export const fonts = {
+  base: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif',
+};
